cb3d1c8c3c73e35e4d364f6ac002b4f7f1374f33
tibmt <pavelyukhnevich@yandex.ru>
2017-09-29 14:21:44 +0300

    'Implement_additional_5'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    function first(bracket, bracketsConfig) {
+        let result;
+        bracketsConfig = bracketsConfig.forEach((element) => {
+            if(bracket == element[0]){
+                result = element[1];
+            }
+        })
+        return result;
+    }
+
+    function second(str, i, j) {
+        return str.slice(0, i) + str.slice(j + 1, str.length);
+    }
+
+    if(str.length == 0){
+        return false
+    }
+    if(str.length % 2 != 0){
+        return false
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i + 1] === first(str[i], bracketsConfig)) {
+            str = second(str, i, i + 1);
+            i = -1;
+            str.length -= 2;
+        }
+    }
+
+    return str.length === 0;
+};
